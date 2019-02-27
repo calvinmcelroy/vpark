7dc731258866a9904af3481e4725368fd99591d2
Calvin McElroy <calvinmcelroy@gmail.com>
2019-02-26 20:06:01 -0600

    Update setup.py

--- labs/nmap/setup.py
+++ labs/nmap/setup.py
@@ -12,22 +12,22 @@ os.system("sudo rm file.txt")
 
 #Install needed config
 if "attacker" in name:
-  os.system("sudo apt-get -y update")
+	os.system("sudo apt-get -y update")
 	os.system("sudo apt-get -y install nmap")
 elif "webserver" in name or "mailserver" in name or "printserver" in name:
 	if "webserver" in name:
 		os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupweb.sh")
-    os.system("sudo chmod 755 setupweb.sh") 
-    os.system("sudo ./setupweb.sh")
-    os.system("sudo rm setupweb.sh")
+    		os.system("sudo chmod 755 setupweb.sh") 
+    		os.system("sudo ./setupweb.sh")
+    		os.system("sudo rm setupweb.sh")
 	elif "mailserver" in name:
 		os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupmail.sh")
-    os.system("sudo chmod 755 setupmail.sh")    
-    os.system("sudo ./setupmail.sh")
-    os.system("sudo rm setupmail.sh")
+    		os.system("sudo chmod 755 setupmail.sh")    
+    		os.system("sudo ./setupmail.sh")
+    		os.system("sudo rm setupmail.sh")
 	elif "printserver" in name:
 		os.system("sudo apt-get -y update")
-    os.system("sudo apt-get -y install samba")
+    		os.system("sudo apt-get -y install samba")
     
 
 		
