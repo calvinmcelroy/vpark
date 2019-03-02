97cd643867aed6b22e09fb1e77a60c17dcc2ec8f
Calvin McElroy <calvinmcelroy@gmail.com>
2019-03-02 00:33:39 -0600

    Update setup.py

--- labs/nmap/setup.py
+++ labs/nmap/setup.py
@@ -14,20 +14,19 @@ os.system("sudo rm file.txt")
 if "attacker" in name:
 	os.system("sudo apt-get -y update")
 	os.system("sudo apt-get -y install nmap")
-elif "webserver" in name or "mailserver" in name or "printserver" in name:
-	if "webserver" in name:
-		os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupweb.sh")
-    		os.system("sudo chmod 755 setupweb.sh") 
-    		os.system("sudo ./setupweb.sh")
-    		os.system("sudo rm setupweb.sh")
-	elif "mailserver" in name:
-		os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupmail.sh")
-    		os.system("sudo chmod 755 setupmail.sh")    
-    		os.system("sudo ./setupmail.sh")
-    		os.system("sudo rm setupmail.sh")
-	elif "printserver" in name:
-		os.system("sudo apt-get -y update")
-    		os.system("sudo apt-get -y install samba")
+if "webserver" in name:
+	os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupweb.sh")
+    	os.system("sudo chmod 755 setupweb.sh") 
+    	os.system("sudo ./setupweb.sh")
+    	os.system("sudo rm setupweb.sh")
+if "mailserver" in name:
+	os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupmail.sh")
+    	os.system("sudo chmod 755 setupmail.sh")    
+    	os.system("sudo ./setupmail.sh")
+    	os.system("sudo rm setupmail.sh")
+if "printserver" in name:
+	os.system("sudo apt-get -y update")
+    	os.system("sudo apt-get -y install samba")
     
 
 		
