61637f6b492c3132925f7771f1f8f9c65e3ff131
Calvin McElroy <calvinmcelroy@gmail.com>
2019-03-15 02:49:17 -0500

    Create setup.py

---
+++ labs/metasploit/setup.py
@@ -0,0 +1,29 @@
+import os
+
+#Get name of machine
+os.system("hostname > file.txt")
+temp_file = open("file.txt", "r")
+name = ""
+for line in temp_file:
+	name = line[:line.index(".")]
+print(name)
+temp_file.close()
+os.system("sudo rm file.txt")
+
+#Install needed config
+if "attacker" in name:
+	os.system("sudo apt-get -y update")
+	os.system("sudo apt-get -y install nmap")
+if "server1" in name:
+	os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupweb.sh")
+    	os.system("sudo chmod 755 setupweb.sh") 
+    	os.system("sudo ./setupweb.sh")
+    	os.system("sudo rm setupweb.sh")
+if "server2" in name:
+	os.system("sudo wget https://raw.githubusercontent.com/calvinmcelroy/vpark/master/labs/nmap/setupmail.sh")
+    	os.system("sudo chmod 755 setupmail.sh")    
+    	os.system("sudo ./setupmail.sh")
+    	os.system("sudo rm setupmail.sh")
+if "server3" in name:
+	os.system("sudo apt-get -y update")
+    	os.system("sudo apt-get -y install samba")
