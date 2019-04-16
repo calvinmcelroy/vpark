079d7166399e05f32210365a9e54e6a5a83245b4
Calvin McElroy <calvinmcelroy@gmail.com>
2019-04-16 02:51:42 -0500

    Create setup.py

---
+++ labs/shells/setup.py
@@ -0,0 +1,18 @@
+import os
+
+#Get name of machine
+os.system("hostname > file.txt")
+temp_file = open("file.txt", "r")
+name = ""
+for line in temp_file:
+	name = line[:line.index(".")]
+print(name)
+temp_file.close()
+os.system("sudo rm file.txt")
+
+#Install needed config
+if "Victim" in name:
+	    os.system("sudo apt-get remove netcat-openbsd")
+    	os.system("sudo apt-get install netcat-traditional") 
+
+
